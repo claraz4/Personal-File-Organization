d2ca27a0d46a4a2b4ddbe8c788477b2c85f73518
Clara Zammar <clarazammar4@gmail.com>
2023-08-16 16:22:29 +0300

    The actions are now kept track of

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import shutil
 from helpers import replace_underscore, is_workout, get_courses, is_uni_file
 
@@ -11,6 +12,14 @@ uni_folder = "C:\\Users\\Admin1\\Desktop\\LAU"
 
 # Get the names of all the files in the downloads directory
 files = os.listdir(downloads)
+
+# Open the file to keep track of the actions done
+actions = open("actions.txt", "a")
+actions.write(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + "\n")
+
+# Check if the downloads file is empty (there's always the desktop.ini
+if len(files) <= 1:
+    actions.write("Downloads folder is empty\n")
 
 
 # Check each file and see whether the conditions fit to move it
@@ -26,27 +35,42 @@ for file in files:
 
     # Get the list of all the courses objects you have (in uni), so I can have the keywords to check for uni files
     courses = get_courses(uni_folder)
+    # Get the course folder if the file a uni file
+    course_folder = is_uni_file(file_name, courses)
 
     # PROGRAMS AND APPLICATIONS
     if extension == ".exe" or extension == ".msi":
         # shutil.move(source_file, destination)
         shutil.move(absolute, programs)
+        actions.write(file_name + " was moved to " + programs + "\n")
 
     # PIANO SHEETS
-    if extension == ".pdf" and "piano" in file_name.lower():
+    elif extension == ".pdf" and "piano" in file_name.lower():
         shutil.move(absolute, piano)
+        actions.write(file_name + " was moved to " + piano + "\n")
 
     # WORKOUT
-    if extension == ".mp4" and is_workout(file_name):
+    elif extension == ".mp4" and is_workout(file_name):
         # Rename the file by changing the underscores
         new_file_name = replace_underscore(file_name)
         new_absolute = os.path.join(downloads, new_file_name + extension)
         os.rename(absolute, new_absolute)
+        if file_name != new_file_name:
+            actions.write(file_name + " was renamed to " + new_file_name + "\n")
 
         # Move the file to the workout folder
-        shutil.move(absolute, workout)
+        shutil.move(new_absolute, workout)
+        actions.write(new_file_name + " was moved to " + workout + "\n")
 
     # LAU FILE (UNIVERSITY)
-    course_folder = is_uni_file(file_name, courses)
-    if course_folder != "":
+    elif course_folder != "":
         shutil.move(absolute, course_folder)
+        actions.write(file_name + " was moved to " + course_folder + "\n")
+
+    else:
+        actions.write(file_name + " still in downloads folder\n")
+
+actions.write("\n")
+actions.write("-" * 20)
+actions.write("\n\n")
+actions.close()
